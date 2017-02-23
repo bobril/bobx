3940a5e7f948385fe86906a7abc92ddada4f7e0e
Boris Letocha <boris.letocha@gmail.com>
2017-02-23 22:30:15 +0100

    Tests for observable depth.

--- spec/ObservableClass.spec.ts
+++ spec/ObservableClass.spec.ts
@@ -1,3 +1,4 @@
+import * as b from 'bobril';
 import { observable } from "../index";
 
 class Person {
@@ -6,6 +7,12 @@ class Person {
 
 class PersonDefault {
     @observable name = "B";
+}
+
+class Depth {
+    @observable.deep deep: { a: string }[];
+    @observable.shallow shallow: { a: string }[];
+    @observable.ref ref: { a: string }[];
 }
 
 describe("ObservableClass", () => {
@@ -33,5 +40,69 @@ describe("ObservableClass", () => {
         expect(pd.name).toBe("B");
         pd.name = "Bobris";
         expect(pd.name).toBe("Bobris");
+    });
+});
+
+function invalidates(observing: () => void, modification: () => void) {
+    let invalidated = false;
+    let invBackup = b.setInvalidate((ctx?: Object, deepness?: number) => { invalidated = true; invBackup(ctx, deepness); });
+    b.init(() => {
+        observing();
+        return "";
+    });
+    b.syncUpdate();
+    invalidated = false;
+    modification();
+    b.setInvalidate(invBackup);
+    return invalidated;
+}
+
+describe("Observable Depth", () => {
+    it("Deep", () => {
+        let o = new Depth();
+        o.deep = [{ a: "A" }];
+        expect(invalidates(() => o.deep, () => o.deep = [{ a: "B1" }])).toBeTruthy();
+        expect(invalidates(() => o.deep[0], () => o.deep = [{ a: "B2" }])).toBeTruthy();
+        expect(invalidates(() => o.deep[0].a, () => o.deep = [{ a: "B3" }])).toBeTruthy();
+
+        expect(invalidates(() => o.deep, () => o.deep[0] = { a: "B4" })).toBeFalsy();
+        expect(invalidates(() => o.deep[0], () => o.deep[0] = { a: "B5" })).toBeTruthy();
+        expect(invalidates(() => o.deep[0].a, () => o.deep[0] = { a: "B6" })).toBeTruthy();
+
+        expect(invalidates(() => o.deep, () => o.deep[0].a = "B7")).toBeFalsy();
+        expect(invalidates(() => o.deep[0], () => o.deep[0].a = "B8")).toBeFalsy();
+        expect(invalidates(() => o.deep[0].a, () => o.deep[0].a = "B9")).toBeTruthy();
+    });
+
+    it("Shallow", () => {
+        let o = new Depth();
+        o.shallow = [{ a: "A" }];
+        expect(invalidates(() => o.shallow, () => o.shallow = [{ a: "B1" }])).toBeTruthy();
+        expect(invalidates(() => o.shallow[0], () => o.shallow = [{ a: "B2" }])).toBeTruthy();
+        expect(invalidates(() => o.shallow[0].a, () => o.shallow = [{ a: "B3" }])).toBeTruthy();
+
+        expect(invalidates(() => o.shallow, () => o.shallow[0] = { a: "B4" })).toBeFalsy();
+        expect(invalidates(() => o.shallow[0], () => o.shallow[0] = { a: "B5" })).toBeTruthy();
+        expect(invalidates(() => o.shallow[0].a, () => o.shallow[0] = { a: "B6" })).toBeTruthy();
+
+        expect(invalidates(() => o.shallow, () => o.shallow[0].a = "B7")).toBeFalsy();
+        expect(invalidates(() => o.shallow[0], () => o.shallow[0].a = "B8")).toBeFalsy();
+        expect(invalidates(() => o.shallow[0].a, () => o.shallow[0].a = "B9")).toBeFalsy();
+    });
+
+    it("Ref", () => {
+        let o = new Depth();
+        o.ref = [{ a: "A" }];
+        expect(invalidates(() => o.ref, () => o.ref = [{ a: "B1" }])).toBeTruthy();
+        expect(invalidates(() => o.ref[0], () => o.ref = [{ a: "B2" }])).toBeTruthy();
+        expect(invalidates(() => o.ref[0].a, () => o.ref = [{ a: "B3" }])).toBeTruthy();
+
+        expect(invalidates(() => o.ref, () => o.ref[0] = { a: "B4" })).toBeFalsy();
+        expect(invalidates(() => o.ref[0], () => o.ref[0] = { a: "B5" })).toBeFalsy();
+        expect(invalidates(() => o.ref[0].a, () => o.ref[0] = { a: "B6" })).toBeFalsy();
+
+        expect(invalidates(() => o.ref, () => o.ref[0].a = "B7")).toBeFalsy();
+        expect(invalidates(() => o.ref[0], () => o.ref[0].a = "B8")).toBeFalsy();
+        expect(invalidates(() => o.ref[0].a, () => o.ref[0].a = "B9")).toBeFalsy();
     });
 });
