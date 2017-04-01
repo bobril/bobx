3b66594af3203d2919b6c2eb3eaa101f5a68b5a3
Boris Letocha <boris.letocha@gmail.com>
2017-04-01 21:55:25 +0200

    New test for prop() on ObservableValue

--- spec/ObservableValue.spec.ts
+++ spec/ObservableValue.spec.ts
@@ -6,7 +6,7 @@ describe("ObservableValue", () => {
         bobx.observable("Hello");
     });
 
-    it("getset", () => {
+    it("get set", () => {
         let v = bobx.observable(42);
         expect(v.get()).toBe(42);
         v.set(1);
@@ -25,5 +25,13 @@ describe("ObservableValue", () => {
         v.set("B");
         expect(invalidated).toBeTruthy();
         b.setInvalidate(invBackup);
+    });
+
+    it("prop", () => {
+        let v = bobx.observable("A");
+        let prop = v.prop();
+        expect(prop()).toBe("A");
+        expect(prop("B")).toBe("B");
+        expect(v.get()).toBe("B");
     });
 });
